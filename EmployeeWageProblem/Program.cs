974dac292c91d4b984d8bea9cec54dd65a2b7000
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-21 14:10:20 +0530

    [sasic] Add . By Using Switch Case Statement to Compute Employee Wages

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -11,17 +11,31 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             const int IS_EMPLOYEE_FULL_TIME = 1;
+            const int IS_EMPLOYEE_PART_TIME = 2;
+            const int EMPLOYEE_RATE_PER_HOUR = 20;
 
-            /// Checks if Employee is present,it will give Present message
-            /// IF Employee not present it will give Absent message
-            if(employeeCheck() == IS_EMPLOYEE_FULL_TIME)
+            int empHrs = 0;
+            int empWage;
+
+            /// By Using Switch Case Statement to Compute Employee Wages
+            /// Checks if Employee is present
+            /// If Employee is Fulltime,it will give Fulltime Employee hours
+            /// If Employee is Parttime,it will give Parttime Employee hours
+            /// If Employee not present it will give Absent Employee hours ie, zero hours.
+            switch(employeeCheck())
             {
-                Console.WriteLine("Employee is Present");
+                case IS_EMPLOYEE_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_EMPLOYEE_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+            empWage = empHrs * EMPLOYEE_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
         }
 
         /// <summary>
@@ -33,7 +47,7 @@ namespace EmployeeWageProblem
             /// Create references for Random class
             Random random = new Random();
             /// Returns random value
-            return random.Next(0, 2);
+            return random.Next(0, 3);
         }
     }
 }
