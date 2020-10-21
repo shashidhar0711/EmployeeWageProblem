5b6f2eec4cd42b8d07a223cd7f1b25cfdc370fd0
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-21 13:54:00 +0530

    [sasic] Add . PartTime Hour to Compute employee Wages

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -11,17 +11,30 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             const int IS_EMPLOYEE_FULL_TIME = 1;
+            const int IS_EMPLOYEE_PART_TIME = 2;
+            const int EMPLOYEE_RATE_PER_HOUR = 20;
 
-            /// Checks if Employee is present,it will give Present message
-            /// IF Employee not present it will give Absent message
-            if(employeeCheck() == IS_EMPLOYEE_FULL_TIME)
+            int empHrs = 0;
+            int empWage;
+
+            /// Checks if Employee is present
+            /// If Employee is Fulltime,it will give Fulltime Employee hours
+            /// If Employee is Parttime,it will give Parttime Employee hours
+            /// If Employee not present it will give Absent Employee hours ie, zero hours.
+            if (employeeCheck() == IS_EMPLOYEE_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else if(employeeCheck() == IS_EMPLOYEE_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMPLOYEE_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
         }
 
         /// <summary>
@@ -33,7 +46,7 @@ namespace EmployeeWageProblem
             /// Create references for Random class
             Random random = new Random();
             /// Returns random value
-            return random.Next(0, 2);
+            return random.Next(0,3);
         }
     }
 }
