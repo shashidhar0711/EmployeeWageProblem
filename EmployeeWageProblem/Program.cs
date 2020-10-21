c95a623321a0aeddefdd23ec51416a1985093cf9
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-21 14:25:35 +0530

    [sasic] Add . Calculating Employee Wages  For a Month

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -11,17 +11,40 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             const int IS_EMPLOYEE_FULL_TIME = 1;
+            const int IS_EMPLOYEE_PART_TIME = 2;
+            const int EMPLOYEE_RATE_PER_HOUR = 20;
+            const int NUM_0F_WORKING_DAYS = 20;
 
-            /// Checks if Employee is present,it will give Present message
-            /// IF Employee not present it will give Absent message
-            if(employeeCheck() == IS_EMPLOYEE_FULL_TIME)
+            int empWage;
+            int empHrs = 0;
+            int totalEmployeeWage = 0;
+
+            /// By Looping untill Condition reaches
+            /// Upto Number of Working Days
+            for (int day = 0; day < NUM_0F_WORKING_DAYS; day++)
             {
-                Console.WriteLine("Employee is Present");
+                /// By Using Switch Case Statement to Compute Employee Wages
+                /// Checks if Employee is present
+                /// If Employee is Fulltime,it will give Fulltime Employee hours
+                /// If Employee is Parttime,it will give Parttime Employee hours
+                /// If Employee not present it will give Absent Employee hours ie, zero hours.
+                switch (employeeCheck())
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMPLOYEE_RATE_PER_HOUR;
+                totalEmployeeWage = totalEmployeeWage + empWage;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+            Console.WriteLine("Employee TotalWage : " +totalEmployeeWage);
         }
 
         /// <summary>
@@ -33,7 +56,7 @@ namespace EmployeeWageProblem
             /// Create references for Random class
             Random random = new Random();
             /// Returns random value
-            return random.Next(0, 2);
+            return random.Next(0, 3);
         }
     }
 }
